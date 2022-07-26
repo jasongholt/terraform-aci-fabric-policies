dc19a059bca3c2a9caa2885f0b39d3573230166f
Jason Holt <jason.grant.holt@gmail.com>
2022-07-26 16:25:27 -0400

    fixed it

--- lacp.tf
+++ lacp.tf
@@ -2,7 +2,6 @@ resource "aci_lacp_policy" "lacp_enabled" {
   description = "LACP Enabled"
   name        = "LACP-Active"
   annotation  = "tag_lacp"
-  ctrl        = "fast-sel-hot-stdby,graceful-conv,susp-individual"
   max_links   = "16"
   min_links   = "1"
   mode        = "active"
@@ -12,8 +11,6 @@ resource "aci_lacp_policy" "lacp_disabled" {
   description = "LACP Disabled"
   name        = "LACP-Disabled"
   annotation  = "tag_lacp"
-  ctrl        = "fast-sel-hot-stdby,graceful-conv"
   max_links   = "16"
   min_links   = "1"
-  mode        = "off"
 }
